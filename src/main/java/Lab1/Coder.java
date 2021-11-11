ae1f99cf752db9598c9ba95b4c0d7fd04968888a
lolwhatvvw <vovkv213@gmail.com>
2021-11-11 19:47:34 +0300

    added entropy

--- src/main/java/Lab1/Coder.java
+++ src/main/java/Lab1/Coder.java
@@ -1,16 +1,17 @@
 package Lab1;
 
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 
 import static Lab1.Huffman.*;
 import static Lab1.MTF.mtfEncode;
 import static Lab1.NBWT.BWT;
 import static Lab1.NBWT.bwt;
+import static java.lang.Math.log;
 
 public class Coder {
 
@@ -39,45 +40,116 @@ public class Coder {
         }
     }
 
+    public static TreeMap<Byte, Double> calc_letter(String s){
+        byte[] b = s.getBytes(StandardCharsets.ISO_8859_1);
+        TreeMap<Byte, Double>  freqMap = new TreeMap<>();
+        for(byte i: b){
+            Double count = freqMap.get(i);
+            freqMap.put(i, count != null? count +1 : 1);}
+        return freqMap;
+    }
+
+    public static TreeMap<Byte, TreeMap<Byte, Double>> calc_letter2(String s){
+        byte[] b = s.getBytes(StandardCharsets.ISO_8859_1);
+        TreeMap<Byte, TreeMap<Byte, Double>> res = new TreeMap<>();
+        boolean bool = false;
+        byte prev = b[0];
+        for (byte i : b){
+            if (!bool){
+                bool = true;
+                continue;
+            }
+            TreeMap<Byte, Double> tm;
+            if (res.containsKey(prev)){
+                tm = res.get(prev);
+                tm.put(i, tm.get(i)!= null? tm.get(i) + 1 : 1);
+            }
+            else{
+                tm = new TreeMap<>();
+                tm.put(i, 1D);
+            }
+            res.put(prev, tm);
+            prev = i;
+        }
+        return res;
+    }
+
+    public static TreeMap<Map<Byte, Byte>, Double> calc_2l(String s){
+        byte[] b = s.getBytes(StandardCharsets.ISO_8859_1);
+        TreeMap<Map<Byte, Byte>, Double> tm = new TreeMap<>();
+        for(int i = 1 ; i< b.length; i++){
+            Map <Byte, Byte> tmp = new HashMap<>(b[i-1],b[i]);
+            tm.put(tmp, tmp!= null? tm.get(tmp) + 1 :1);
+        }
+        return tm;
+    }
+
+    /*    TODO: Implement Map comparable to check contains*/
+    public static TreeMap<Map<Byte, Byte>, TreeMap<Byte, Double>> calc_2cnt (String s){
+        byte[] b = s.getBytes(StandardCharsets.ISO_8859_1);
+        TreeMap<Map<Byte, Byte>, TreeMap<Byte, Double>> tm = new TreeMap<>();
+        for (int i = 2; i < b.length; i++){
+            Map <Byte, Byte> pr = new HashMap<>(b[i-2],b[i-1]);
+            TreeMap<Byte, Double> mp ;
+            if (tm.containsKey(pr)) {
+                mp = tm.get(pr);
+                Double tmp = mp != null? mp.get(b[i] + 1) : 1D;
+                mp.put(b[i], tmp);
+            } else {
+                mp = new TreeMap<>();
+                mp.put(b[i], 1D);
+            }
+            tm.put(pr, mp);
+        }
+        return tm;
+    }
+
+    public static double entropy_HX(String s){
+        TreeMap<Byte, Double> letter1cnt = calc_letter(s);
+        double entropy_HX = 0;
+        for(Map.Entry<Byte, Double> mp : letter1cnt.entrySet()){
+            entropy_HX -= mp.getValue()/s.length() * log(mp.getValue()/s.length()) / log(2);
+        }
+        return entropy_HX;
+    }
+
+    public static double HIH(String s){
+        TreeMap<Byte, Double> letter1cnt = calc_letter(s);
+        TreeMap<Byte, TreeMap<Byte, Double>> letter2cnt = calc_letter2(s);
+        double entropy_HIH = 0;
+        for (Map.Entry<Byte, Double> e : letter1cnt.entrySet()){
+            double sum = 0;
+            double py = e.getValue() / s.length();
+            for (Map.Entry<Byte, Double> e2 : letter2cnt.get(e.getKey()).entrySet()){
+                double pxy = (double) e2.getValue() / e.getValue();
+                sum += pxy * Math.log(pxy) / Math.log(2);
+            }
+            entropy_HIH -= py * sum;
+        }
+        return entropy_HIH;
+    }
+
+    public static double calc_entropy_hXIXX(String s){
+        TreeMap<Map<Byte, Byte>, TreeMap<Byte, Double>> tmp = calc_2cnt(s);
+        TreeMap<Map<Byte, Byte>, Double> tp = calc_2l(s);
+
+        double entropy_HXIXX = 0;
+        for (Map.Entry<Map<Byte, Byte>, Double> e: tp.entrySet()){
+            double sum = 0;
+            double py =  e.getValue() / s.length() ;
+            for (Map.Entry<Byte, Double> ew: tmp.get(e.getKey()).entrySet()){
+                double pxy = ew.getValue()/e.getValue();
+                sum+= pxy * log(pxy) / log(2);
+            }
+            entropy_HXIXX -= py * sum;
+        }
+        return entropy_HXIXX;
+    }
+
+
     public static void main(String[] args) {
 
-        Path p = Paths.get(args[0]);
-
-        StringBuilder content = new StringBuilder();
-
-        try(FileInputStream reader = new FileInputStream(String.valueOf(p)))
-        {
-            int c;
-            while((c= reader.read())!=-1){
-                content.append((char) c);
-            }
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
-
-        BWT bwt = bwt(content.toString());
-
-        List<Integer> ls = new ArrayList<>(mtfEncode(bwt.res));
-
-        TreeMap<Integer, Integer> frequancies = frequancy(ls);
-        ArrayList<TreeNode> treeNodes = new ArrayList<>();
-        for(Integer c: frequancies.keySet()){
-            treeNodes.add(new TreeNode(c, frequancies.get(c)));
-        }
-
-        TreeNode tree = huffman(treeNodes);
-
-        TreeMap<Integer, String> codes = new TreeMap<>();
-        for(Integer c: frequancies.keySet()){
-            codes.put(c, tree.getCode(c, ""));
-        }
-        String encoded = encode(codes, ls);
 
 
-
-        File file = new File(args[1]);
-
-        saveToFile(file, bwt.index, frequancies, encoded);
     }
 }
